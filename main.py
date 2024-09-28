9d228593e914f05f07bebb67ec715a45922c24a9
TheRayGent <egortitanpc@gmail.com>
2024-09-28 20:44:31 +0200

    test1

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ cfg = ConfigParser()
 
 
 
-config_text=requests.get('https://raw.githubusercontent.com/TheRayGent/project-localization/refs/heads/main/localization/citadel_gc/citadel_gc_russian.txt').text
+config_text=requests.get('https://raw.githubusercontent.com/TheRayGent/project-localization/refs/heads/main/config.cfg').text
 
 
 
