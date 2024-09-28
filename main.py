8a4d6a4c86b1b679da0a1a38f4a996fa3d40dfa2
TheRayGent <egortitanpc@gmail.com>
2024-09-29 00:33:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,21 @@ from configparser import ConfigParser
 import requests
 cfg = ConfigParser()
 
-#text = requests.get('https://raw.githubusercontent.com/TheRayGent/project-localization/refs/heads/main/localization/citadel_gc/citadel_gc_russian.txt').text
-
-a = 'D:/SteamLibrary/steamapps/common/Deadlock/game/citadel/resource'
-
+#a = 'D:\\SteamLibrary\\steamapps\\common\\Deadlock'
+a = input('Введите путь до папки игры (Пример: D:/SteamLibrary/steamapps/common/Deadlock ): ').strip()
+if a[-2:-1]=='\\' or a[-2:-1]=='/':
+    a += 'game/citadel/resource'
+else: a += '/game/citadel/resource'
 config_text=requests.get('https://raw.githubusercontent.com/TheRayGent/project-localization/refs/heads/main/config.cfg').text
 
 
 
 cfg.read_string(config_text)
-#cfg.read('config.cfg')
 
 for i in cfg.items('EditFiles'):
     b = a+i[1]
     c = requests.get('https://raw.githubusercontent.com/TheRayGent/project-localization/refs/heads/main'+i[1]).text
-    print(b)
+    print(b,end='')
     txt = open(a+i[1], encoding='utf_8_sig', mode='w')
     txt.write(c)
     txt = open(a+i[1], encoding='utf_8_sig')
@@ -26,10 +26,6 @@ for i in cfg.items('EditFiles'):
         else: c1+=j
     txt = open(a+i[1], encoding='utf_8_sig', mode='w')
     txt.write(c1)
-    
-
-
-'''with open('ChromeCacheView.cfg', 'w') as config_file:
-    cfg.write(config_file)
-txt = open('citadel_gc_russian.txt', encoding='utf_8_sig', mode='w')
-txt.write(a)'''
+    print(' - успешно')
+print('Кастомный перевод установлен успешно!')
+input()
