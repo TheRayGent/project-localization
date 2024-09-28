3d4016a85f359f9b0caf8146c78aedf77d0fa7d2
TheRayGent <egortitanpc@gmail.com>
2024-09-28 23:08:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,21 @@ cfg = ConfigParser()
 
 #text = requests.get('https://raw.githubusercontent.com/TheRayGent/project-localization/refs/heads/main/localization/citadel_gc/citadel_gc_russian.txt').text
 
-a = 'D:\\SteamLibrary\\steamapps\\common\\Deadlock'
+a = 'D:/SteamLibrary/steamapps/common/Deadlock/game/citadel/resource'
 
 config_text=requests.get('https://raw.githubusercontent.com/TheRayGent/project-localization/refs/heads/main/config.cfg').text
 
 
 
 cfg.read_string(config_text)
+#cfg.read('config.cfg')
 
 for i in cfg.items('EditFiles'):
-    b = a+i[0]
+    b = a+i[1]
     c = requests.get('https://raw.githubusercontent.com/TheRayGent/project-localization/refs/heads/main'+i[1]).text
-    print(i[0])
+    print(b)
+    txt = open(a+i[1], encoding='utf_8_sig', mode='w')
+    txt.write(c)
 
 
 
